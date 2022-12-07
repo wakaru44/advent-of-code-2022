6fa424a86c026de6159cdd9dc449a3ee355cf13a
Wakaru Himura <wakaru44@gmail.com>
2022-12-07 22:39:37 +0100

    fuck day 05

--- day_05/README.md
+++ day_05/README.md
@@ -1,38 +1,59 @@
-Day 3: Rucksack Reorganization
-==============================
+# Day 5 Supply Stacks
 
-One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, that Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
 
-Each **rucksack** has two large **compartments**. All items of a given type are meant to go into exactly one of the two compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
+## Part 1
 
-The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your help finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, `a` and `A` refer to different types of items).
+The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in **stacks** of marked crates, but because the needed supplies are buried under many other crates, the crates need to be rearranged.
 
-The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same number of items in each of its two compartments, so the _first half of the characters represent items in the first compartment_, while the second half of the characters represent items in the second compartment.
+The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
 
-For example, suppose you have the following list of contents from six rucksacks:
+The Elves don't want to interrupt the crane operator during this delicate procedure, but they forgot to ask her which crate will end up where, and they want to be ready to unload them as soon as possible so they can embark.
 
-    vJrwpWtwJgWrhcsFMMfFFhFp
-    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-    PmmdzqPrVvPwwTWBwg
-    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-    ttgJtRGJQctTZtZT
-    CrZsJsPPZsGzwwsLwLmpwMDw
+They do, however, have a **drawing of the starting stacks of crates and the rearrangement procedure** (your puzzle input). For example:
 
-- The first rucksack contains the items `vJrwpWtwJgWrhcsFMMfFFhFp`, which means its first compartment contains the items `vJrwpWtwJgWr`, while the second compartment contains the items `hcsFMMfFFhFp`. The only item type that appears in both compartments is lowercase p.
-- The second rucksack's compartments contain `jqHRNqRjqzjGDLGL` and `rsFMfFZSrLrFZsSL`. The only item type that appears in both compartments is uppercase L.
-- The third rucksack's compartments contain `PmmdzqPrV` and `vPwwTWBwg`; the only common item type is uppercase P.
-- The fourth rucksack's compartments only share item type v.
-- The fifth rucksack's compartments only share item type t.
-- The sixth rucksack's compartments only share item type s.
+        [D]    
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
 
-To help prioritize item rearrangement, every item type can be converted to a priority:
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2
 
-- Lowercase item types a through z have priorities 1 through 26.
+In this example, there are three stacks of crates. Stack 1 contains two crates: crate Z is on the bottom, and crate N is on top. Stack 2 contains three crates; from bottom to top, they are crates M, C, and D. Finally, stack 3 contains a single crate, P.
 
-- Uppercase item types A through Z have priorities 27 through 52.
+Then, the rearrangement procedure is given. In each step of the procedure, a quantity of crates is moved from one stack to a different stack. In the first step of the above rearrangement procedure, one crate is moved from stack 2 to stack 1, resulting in this configuration:
 
-In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
+    [D]        
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
 
-Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+In the second step, three crates are moved from stack 1 to stack 3. Crates are moved one at a time, so the first crate to be moved (D) ends up below the second and third crates:
 
------------
+            [Z]
+            [N]
+        [C] [D]
+        [M] [P]
+    1   2   3
+
+Then, both crates are moved from stack 2 to stack 1. Again, because crates are moved one at a time, crate C ends up below crate M:
+
+            [Z]
+            [N]
+    [M]     [D]
+    [C]     [P]
+    1   2   3
+
+Finally, one crate is moved from stack 1 to stack 2:
+
+            [Z]
+            [N]
+            [D]
+    [C] [M] [P]
+    1   2   3
+
+The Elves just need to know which crate will end up on top of each stack; in this example, the top crates are C in stack 1, M in stack 2, and Z in stack 3, so you should combine these together and give the Elves the message CMZ.
+
+After the rearrangement procedure completes, what crate ends up on top of each stack?
--- day_05/dayfaif.py
+++ day_05/dayfaif.py
@@ -1,4 +1,284 @@
 
 
-if __name__ == "__main__":
+test_data = "move 99 from 9 to 9"
+
+test_moves = """
+             move 1 from 2 to 1
+             move 3 from 1 to 3
+             move 2 from 2 to 1
+             move 1 from 1 to 2
+             """
+
+test_stack = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+"""
+
+index = " 1   2   3   4   5   6   7   8   9 "
+
+
+def get_col(col, index=index):
+    """
+    Get the position where the column number col is located
+
+    >>> get_col(1)
+    1
+
+    """
+    return index.find(str(col))
+
+
+def get_stack(fname="input_stack.csv", test_data=None):
+    """ transpose the stack, clean it, make it nice.
+
+    >>> get_stack(test_data=test_stack)
+    {1: [' ', 'N', 'Z'], 2: ['D', 'C', 'M'], 3: [' ', ' ', 'P']}
+
+    """
+    raw = read_stack(fname, test_data)
+    trans = transpose(raw)
+    # transpose is not enough, we have to retrieve only the good stuff
+    stack = {}
+    for i in range(1, 10):  # numbers 1 to 9
+        try:
+            stack[i] = trans[get_col(i)]
+        except IndexError:
+            pass  # We can ignore stacks that are too small for our index.
+    return stack
+
+
+def read_stack(fname="input_stack.csv", test_data=None):
+    """
+    Load the stack matrix. from the file
+
+    >>> read_stack(test_data=test_stack)
+    ['    [D]    ', '[N] [C]    ', '[Z] [M] [P]']
+
+    """
+    if test_data is not None:
+        it = test_data.split("\n")
+    else:
+        it = open(fname, "r")
+
+    stack = []  # The stack is a list of lists.
+    index = ""  # The index is then a string? or can it be a list?
+
+    for line in it:
+        # cline = line.strip() # In this case, it can't be stripped of the whitespaces.
+        cline = line.replace("\n", "")  # just remove them manually.
+        # If is the index, put it in the index
+        if cline.startswith(" 1") or "1" in cline:
+            index = cline
+        # if the line is empty, ignore it.
+        elif cline != '':
+            stack.append(cline)
+        else:
+            continue
+    if test_data is None:
+        it.close()  # we are not using with, so we must close manually.
+    return stack
+
+
+def encode_moves(line):
+    """Get a line of moves and encode them in a tuple
+
+    >>> encode_moves('move 1 from 2 to 1')
+    {'move': 1, 'src': 2, 'dst': 1}
+
+    >>> encode_moves('move 11 from 9 to 11')
+    {'move': 11, 'src': 9, 'dst': 11}
+    """
+
+    trozos = line.split(" ")
+    encoded = {
+        'move': int(trozos[1]),
+        'src':  int(trozos[3]),
+        'dst':  int(trozos[5])
+    }
+    return encoded
+
+
+def get_moves(fname="input_moves.csv", test_data=None):
+    """
+    Load the file, and process the data a little bit.
+
+    >>> get_moves(test_data=test_data).__next__()   # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+    {'move': 99, 'src': 9, 'dst': 9}
+
+    >>> get_moves(test_data=test_moves).__next__()   # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+    {'move': 1, 'src': 2, 'dst': 1}
+
+
+    """
+    if test_data is not None:
+        it = test_data.split("\n")
+    else:
+        it = open(fname, "r")
+    for line in it:
+        cline = line.strip()
+        if cline != '':  # if the line is empty, ignore it.
+            try:
+                yield encode_moves(cline)
+            except ValueError as e:
+                print("FUCK {e}")
+                print(cline)
+        else:
+            continue
+    if test_data is None:
+        it.close()  # we are not using with, so we must close manually.
+
+
+def transpose(matrix):
+    """
+    Transpose a list of lists.
+
+    >>> transpose([[1,2],[3,4],[5,6]])
+    [[1, 3, 5], [2, 4, 6]]
+    """
+    return list(map(list, zip(*matrix)))
+
+
+def take_one(col, st="WTF"):
+    """
+    get the top item on this column. return a tuple of the item and the new stack
+
+    >>> take_one(2, get_stack(test_data=test_stack) )
+    ('D', {1: [' ', 'N', 'Z'], 2: [' ', 'C', 'M'], 3: [' ', ' ', 'P']})
+
+    """
+    assert st is not None
+    hand = ''
+    for pos, item in enumerate(st[col]):
+        if item == ' ':  # the item is empty, go on to the next pos.
+            continue
+        else:  # the item is 'something', so we must take it out.
+            hand = item
+            st[col][pos] = ' '
+            # and we are done
+            return (hand, st)
+    #print(f"DEBUG: {st} - {col}")
+    print("OOPSIETAKE")  # we should never get here.
+
+
+def put_one(col, hand, stack=None):
+    """
+    Put one item from the hand to the stack
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> (hand,st) = take_one(2,st)
+    >>> put_one(2,hand,st)
+    {1: [' ', 'N', 'Z'], 2: ['D', 'C', 'M'], 3: [' ', ' ', 'P']}
+
+    """
+    assert stack is not None
+    # print("DEBUG STACK ",stack) # FIXED: the stack was created with just 8.
+    for pos, item in enumerate(stack[col]):
+        if item == ' ':  # we don't care about empty
+            continue
+        else:  # The first one we hit, we do the did.
+            if pos == 0:  # If we need to enlarge the pile
+                stack[col].insert(0, hand)
+            else:
+                prev = pos-1
+                stack[col][prev] = hand
+            return stack
+    # If we get here, means we are at the end of the pile.
+    # append to the end and return the stack.
+    stack[col].append(hand)
+    return stack
+
+
+def do_move(order, stack=None):
+    """
+    Perform the operations for a given move.
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> do_move({'move':1, 'src': 2, 'dst':1},st)
+    {1: ['D', 'N', 'Z'], 2: [' ', 'C', 'M'], 3: [' ', ' ', 'P']}
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> moves = list(get_moves(test_data=test_moves))
+    >>> do_move(moves[0],st)
+    {1: ['D', 'N', 'Z'], 2: [' ', 'C', 'M'], 3: [' ', ' ', 'P']}
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> moves = list(get_moves(test_data=test_moves))
+    >>> st2 = do_move(moves[0],st)
+    >>> do_move(moves[1],st2)
+    {1: [' ', ' ', ' '], 2: [' ', 'C', 'M'], 3: ['Z', 'N', 'D', 'P']}
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> moves = list(get_moves(test_data=test_moves))
+    >>> st = do_move(moves[0],st)
+    >>> st = do_move(moves[1],st)
+    >>> #print(st)
+    >>> do_move(moves[2],st)
+    {1: [' ', ' ', 'M', 'C'], 2: [' ', ' ', ' '], 3: ['Z', 'N', 'D', 'P']}
+
+
+    """
+    assert stack is not None
+
+    for n in range(order['move']):
+        # print("DEBUGIOE",n, order['move']) # DAMAGE: I fucked up and returend too early.
+        try:
+            (hand, stack) = take_one(order['src'], stack)
+            stack = put_one(order['dst'], hand, stack)
+        except TypeError as e:
+            print(f"DEBUG: {stack} - {order}")
+            print(e)
+            raise TypeError(e)
+        except AssertionError as e:
+            # Catch the assertions of put and take to debug.
+            print(f"DEBUGAS: {stack} - {order}")
+            print(e)
+            #raise AssertionError(e)
+    return stack  # Return the stack as it is updated.
+
+
+def untranspose(stack):
+    r"""
+    un transpose the stack back into a vertical pile
+
+    >>> untranspose({1:[1,2,3],2:[4,5,6]})
+    [[1,2,3],[4,5,6]]
+
+    >>> st = get_stack(test_data=test_stack)
+    >>> untranspose(st)
+    ' [ ] [D] [ ]\n [N] [C] [ ]\n [Z] [M] [P]'
+    """
+    horizontal = []
+    for k, v in stack.items():
+        horizontal.append(list(map(
+            lambda x: f" [{x}]",
+            v
+        )))
+    vertical = transpose(horizontal)
+    return "\n".join(["".join(y) for y in vertical])
+
+
+def part1(data=None):
+    """Run all the moves through the stack"""
+    stack = get_stack()
+    print("Before")
+    print(untranspose(stack))
+    moves = get_moves()
+    for move in moves:
+        stack = do_move(move, stack)
+    print("After")
+    print(untranspose(stack))
+    return "Nice"
+
+
+def part2(data=None):
     pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    print("Results:")
+    print(part1(get_moves()))
+    print(part2(get_moves()))
